0b359c593fa924147ab25e405f0f6209799ae445
Operations Research Engineering Software+ <alex@oresoftware.com>
2018-12-24 13:57:59 -0800

    ores/auto-commit => 'set'

--- scripts/eb/deploy.sh
+++ scripts/eb/deploy.sh
@@ -3,8 +3,8 @@
 export PATH="$HOME/.local/bin:${PATH}"  # eb is installed here on linux
 
 export access_id="$huru_access_id"
-export access_key="$huru_access_key" #  0fQhN7QjaeD8DPc0AW/owiqvdscvTc/gYk1vVbsB
+export access_key="$huru_access_key" #
 
 eb codesource local
-eb deploy --staged huru-api2-dev #HuruApi-env-1 #huru-api-dev2 # e-c72mgj2tmr # e-zp2ptw28nv
+eb deploy --staged huru-api2-dev
 
